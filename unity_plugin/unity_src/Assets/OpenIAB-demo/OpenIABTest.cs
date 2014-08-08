f4015131db019749b501334d4f45d1e60b822211
Alexey Karazhey <grimreio@gmail.com>
2014-08-08 14:23:44 +0300

    demo ui update

--- unity_plugin/unity_src/Assets/OpenIAB-demo/OpenIABTest.cs
+++ unity_plugin/unity_src/Assets/OpenIAB-demo/OpenIABTest.cs
@@ -24,16 +24,9 @@ using System.Collections.Generic;
 public class OpenIABTest : MonoBehaviour
 {
     const string SKU = "sku";
-    const string SKU_AMMO = "sku_ammo_general";
-    const string SKU_MEDKIT = "sku_medkit_general";
-    const string SKU_SUBSCRIPTION = "sku_sub_general";
-    const string SKU_INFINITE_AMMO = "sku_inf_ammo_general";
-    const string SKU_COWBOY_HAT = "sku_hat_general";
 
-#pragma warning disable 0414
     string _label = "";
     bool _isInitialized = false;
-#pragma warning restore 0414
 
     private void OnEnable()
     {
@@ -62,50 +55,63 @@ public class OpenIABTest : MonoBehaviour
 
     private void Start()
     {
-        // Map skus for different stores
-        OpenIAB.mapSku(SKU_MEDKIT, OpenIAB_iOS.STORE, "30_real");
-        OpenIAB.mapSku(SKU_AMMO, OpenIAB_iOS.STORE, "75_real");
-        OpenIAB.mapSku(SKU_INFINITE_AMMO, OpenIAB_iOS.STORE, "noncons_2");
-        OpenIAB.mapSku(SKU_COWBOY_HAT, OpenIAB_iOS.STORE, "noncons_1");
-
+        // Map skus for different stores       
+        OpenIAB.mapSku(SKU, OpenIAB_Android.STORE_GOOGLE, "sku");
         OpenIAB.mapSku(SKU, OpenIAB_iOS.STORE, "sku");
-
-        OpenIAB.mapSku(SKU_AMMO, OpenIAB_WP8.STORE, "sku_ammo");
-        OpenIAB.mapSku(SKU_MEDKIT, OpenIAB_WP8.STORE, "sku_medkit");
-        OpenIAB.mapSku(SKU_SUBSCRIPTION, OpenIAB_WP8.STORE, "sku_sub");
+        OpenIAB.mapSku(SKU, OpenIAB_WP8.STORE, "sku");
     }
 
-#if UNITY_ANDROID
-    
-    const float SCREEN_XY_OFFSET = 5.0f;
+    const float X_OFFSET = 10.0f;
     const float Y_OFFSET = 10.0f;
     const int SMALL_SCREEN_SIZE = 800;
     const int LARGE_FONT_SIZE = 34;
-    const int SMALL_FONT_SIZE = 34;
+    const int SMALL_FONT_SIZE = 24;
     const int LARGE_WIDTH = 380;
     const int SMALL_WIDTH = 160;
     const int LARGE_HEIGHT = 100;
     const int SMALL_HEIGHT = 40;
 
+    int _column = 0;
+    int _row = 0;
+
+    private bool Button(string text)
+    {
+        float width = Screen.width / 2.0f - X_OFFSET * 2;
+        float height = (Screen.width >= SMALL_SCREEN_SIZE || Screen.height >= SMALL_SCREEN_SIZE) ? LARGE_HEIGHT : SMALL_HEIGHT;
+        
+        bool click = GUI.Button(new Rect(
+            X_OFFSET + _column * X_OFFSET * 2 + _column * width, 
+            Y_OFFSET + _row * Y_OFFSET + _row * height, 
+            width, height),
+            text);
+
+        ++_column;
+        if (_column > 1)
+        {
+            _column = 0;
+            ++_row;
+        }
+
+        return click;
+    }
+
     private void OnGUI()
     {
-        float yPos = SCREEN_XY_OFFSET;
-        float xPos = SCREEN_XY_OFFSET;
+        _column = 0;
+        _row = 0;
+        
         GUI.skin.button.fontSize = (Screen.width >= SMALL_SCREEN_SIZE || Screen.height >= SMALL_SCREEN_SIZE) ? LARGE_FONT_SIZE : SMALL_FONT_SIZE;
-        float width = (Screen.width >= SMALL_SCREEN_SIZE || Screen.height >= SMALL_SCREEN_SIZE) ? LARGE_WIDTH : SMALL_WIDTH;
-        float height = (Screen.width >= SMALL_SCREEN_SIZE || Screen.height >= SMALL_SCREEN_SIZE) ? LARGE_HEIGHT : SMALL_HEIGHT;
-        float heightPlus = height + Y_OFFSET;
 
-        if (GUI.Button(new Rect(xPos, yPos, width, height), "Initialize OpenIAB"))
+        if (Button("Initialize OpenIAB"))
         {
             // Application public key
-            var public_key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAqibEPHCtfPm3Rn26gbE6vhCc1d6A072im+oWNlkUAJYV//pt1vCkYLqkkw/P2esPSWaw1nt66650vfVYc3sYY6L782n/C+IvZWQt0EaLrqsSoNfN5VqPhPeGf3wqsOvbKw9YqZWyKL4ddZUzRUPex5xIzjHHm3qIJI5v7iFJHOxOj0bLuEG8lH0Ljt/w2bNe4o0XXoshYDqpzIKmKy6OYNQOs8iBTJlfSmPrlGudmldW6CsuAKeVGm+Z+2xx3Xxsx3eSwEgEaUc1ZsMWSGsV6dXgc3JrUvK23JRJUu8X5Ec1OQLyxL3VelD5f0iKVTJ1kw59tMAVZ7DDpzPggWpUkwIDAQAB";
+            var publicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAqibEPHCtfPm3Rn26gbE6vhCc1d6A072im+oWNlkUAJYV//pt1vCkYLqkkw/P2esPSWaw1nt66650vfVYc3sYY6L782n/C+IvZWQt0EaLrqsSoNfN5VqPhPeGf3wqsOvbKw9YqZWyKL4ddZUzRUPex5xIzjHHm3qIJI5v7iFJHOxOj0bLuEG8lH0Ljt/w2bNe4o0XXoshYDqpzIKmKy6OYNQOs8iBTJlfSmPrlGudmldW6CsuAKeVGm+Z+2xx3Xxsx3eSwEgEaUc1ZsMWSGsV6dXgc3JrUvK23JRJUu8X5Ec1OQLyxL3VelD5f0iKVTJ1kw59tMAVZ7DDpzPggWpUkwIDAQAB";
 
             var options = new Options();
             options.verifyMode = OptionsVerifyMode.VERIFY_SKIP;
             options.prefferedStoreNames = new string[] { OpenIAB_Android.STORE_GOOGLE };
             options.storeKeys = new Dictionary<string, string> {
-                {OpenIAB_Android.STORE_GOOGLE, public_key}
+                {OpenIAB_Android.STORE_GOOGLE, publicKey}
             };
 
             // Transmit options and start the service
@@ -115,91 +121,44 @@ public class OpenIABTest : MonoBehaviour
         if (!_isInitialized)
             return;
 
-        if (GUI.Button(new Rect(xPos, yPos += heightPlus, width, height), "Test Purchase"))
-        {
-            OpenIAB.purchaseProduct("android.test.purchased");
-        }
-
-        if (GUI.Button(new Rect(xPos, yPos += heightPlus, width, height), "Test Refund"))
-        {
-            OpenIAB.purchaseProduct("android.test.refunded");
-        }
-
-        if (GUI.Button(new Rect(xPos, yPos += heightPlus, width, height), "Test Item Unavailable"))
-        {
-            OpenIAB.purchaseProduct("android.test.item_unavailable");
-        }
-
-        xPos = Screen.width - width - SCREEN_XY_OFFSET;
-        yPos = SCREEN_XY_OFFSET;
-
-        if (GUI.Button(new Rect(xPos, yPos, width, height), "Test Purchase Canceled"))
-        {
-            OpenIAB.purchaseProduct("android.test.canceled");
-        }
-
-        if (GUI.Button(new Rect(xPos, yPos += heightPlus, width, height), "Query Inventory"))
+        if (Button("Query Inventory"))
         {
             OpenIAB.queryInventory(new string[] { SKU });
         }
 
-        if (GUI.Button(new Rect(xPos, yPos += heightPlus, width, height), "Purchase Real Product"))
+        if (Button("Purchase Product"))
         {
             OpenIAB.purchaseProduct(SKU);
         }
 
-        if (GUI.Button(new Rect(xPos, yPos += heightPlus, width, height), "Consume Real Product"))
+        if (Button("Consume Product"))
         {
             OpenIAB.consumeProduct(Purchase.CreateFromSku(SKU));
         }
-    }
+
+// Android specific buttons
+#if UNITY_ANDROID
+        if (Button("Test Purchase"))
+        {
+            OpenIAB.purchaseProduct("android.test.purchased");
+        }
+
+        if (Button("Test Refund"))
+        {
+            OpenIAB.purchaseProduct("android.test.refunded");
+        }
+
+        if (Button("Test Item Unavailable"))
+        {
+            OpenIAB.purchaseProduct("android.test.item_unavailable");
+        }
+
+        if (Button("Test Purchase Canceled"))
+        {
+            OpenIAB.purchaseProduct("android.test.canceled");
+        }
 #endif
-
-#if UNITY_IOS
-    const int CONTROL_OFFSET = 10;
-    const float SCREEN_Y_MULT = 0.1f;
-    const float SCREEN_X_MULT = 0.3f;
-
-    void OnGUI()
-    {
-        if (GUI.Button(new Rect(CONTROL_OFFSET, CONTROL_OFFSET, Screen.width * SCREEN_X_MULT, Screen.height * SCREEN_Y_MULT), "Init"))
-        {
-            OpenIAB.init(new Options());
-        }
-        if (GUI.Button(new Rect(CONTROL_OFFSET * 2 + Screen.width * SCREEN_X_MULT, CONTROL_OFFSET, Screen.width * SCREEN_X_MULT, Screen.height * SCREEN_Y_MULT), "Query inventory"))
-        {
-            OpenIAB.queryInventory(new string[] { SKU_MEDKIT });
-        }
-        if (GUI.Button(new Rect(CONTROL_OFFSET * 3 + Screen.width * SCREEN_X_MULT * 2, CONTROL_OFFSET, Screen.width * SCREEN_X_MULT, Screen.height * SCREEN_Y_MULT), "Purchase"))
-        {
-            OpenIAB.purchaseProduct(SKU_MEDKIT);
-        }
-        GUI.Label(new Rect(CONTROL_OFFSET, CONTROL_OFFSET * 2 + Screen.height * SCREEN_Y_MULT, Screen.width, Screen.height), _label);
     }
-#endif
-
-#if UNITY_WP8
-    const int BUTTON_OFFSET = 10;
-    const float SCREEN_Y_MULT = 0.1f;
-    const float SCREEN_X_MULT = 0.3f;
-
-    void OnGUI()
-    {
-        if (GUI.Button(new Rect(BUTTON_OFFSET, BUTTON_OFFSET, Screen.width * SCREEN_X_MULT, Screen.height * SCREEN_Y_MULT), "QUERY INVENTORY"))
-        {
-            OpenIAB.queryInventory(new string[] { SKU_AMMO, SKU_MEDKIT, SKU_SUBSCRIPTION });
-        }
-        if (GUI.Button(new Rect(BUTTON_OFFSET * 2 + Screen.width * SCREEN_X_MULT, BUTTON_OFFSET, Screen.width * SCREEN_X_MULT, Screen.height * SCREEN_Y_MULT), "Purchase"))
-        {
-            OpenIAB.purchaseProduct(SKU_MEDKIT);
-        }
-        if (GUI.Button(new Rect(BUTTON_OFFSET * 3 + Screen.width * SCREEN_X_MULT * 2, BUTTON_OFFSET, Screen.width * SCREEN_X_MULT, Screen.height * SCREEN_Y_MULT), "Consume"))
-        {
-            OpenIAB.consumeProduct(Purchase.CreateFromSku(SKU_MEDKIT));
-        }
-        GUI.Label(new Rect(BUTTON_OFFSET, BUTTON_OFFSET * 2 + Screen.height * SCREEN_Y_MULT, Screen.width, Screen.height), _label);
-    }
-#endif
 
     private void billingSupportedEvent()
     {
