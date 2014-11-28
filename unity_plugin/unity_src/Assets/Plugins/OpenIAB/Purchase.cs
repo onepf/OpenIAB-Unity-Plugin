371228fe0dc8e8deef59612ad49dee9bd52e9471
reio <grimreio@gmail.com>
2014-11-28 18:34:00 +0200

    purchase struct fields description #28

--- unity_plugin/unity_src/Assets/Plugins/OpenIAB/Purchase.cs
+++ unity_plugin/unity_src/Assets/Plugins/OpenIAB/Purchase.cs
@@ -21,16 +21,49 @@ namespace OnePF
      */
     public class Purchase
     {
-        public string ItemType { get; private set; }  // ITEM_TYPE_INAPP or ITEM_TYPE_SUBS
+        /// <summary>
+        /// ITEM_TYPE_INAPP or ITEM_TYPE_SUBS
+        /// </summary>
+        public string ItemType { get; private set; }
+        /// <summary>
+        /// A unique order identifier for the transaction. This corresponds to the Google Wallet Order ID.
+        /// </summary>
         public string OrderId { get; private set; }
+        /// <summary>
+        /// The application package from which the purchase originated.
+        /// </summary>
         public string PackageName { get; private set; }
+        /// <summary>
+        /// The item's product identifier. Every item has a product ID, which you must specify in the application's product list on the Google Play Developer Console.
+        /// </summary>
         public string Sku { get; private set; }
+        /// <summary>
+        /// The time the product was purchased, in milliseconds since the epoch (Jan 1, 1970).
+        /// </summary>
         public long PurchaseTime { get; private set; }
+        /// <summary>
+        /// The purchase state of the order. Possible values are 0 (purchased), 1 (canceled), or 2 (refunded).
+        /// </summary>
         public int PurchaseState { get; private set; }
+        /// <summary>
+        /// A developer-specified string that contains supplemental information about an order. You can specify a value for this field when you make a getBuyIntent request.
+        /// </summary>
         public string DeveloperPayload { get; private set; }
+        /// <summary>
+        /// A token that uniquely identifies a purchase for a given item and user pair. 
+        /// </summary>
         public string Token { get; private set; }
+        /// <summary>
+        /// JSON sent by the current store
+        /// </summary>
         public string OriginalJson { get; private set; }
+        /// <summary>
+        /// Signature of the JSON string
+        /// </summary>
         public string Signature { get; private set; }
+        /// <summary>
+        /// Current store name
+        /// </summary>
         public string AppstoreName { get; private set; }
 
         private Purchase()
